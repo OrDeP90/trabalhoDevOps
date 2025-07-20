a5028d92e7dc196db73fe713e2cb29ca4a068028
Pedro Henrique Ghiotto <pedro.ghiotto@catijr.com.br>
2025-07-20 15:30:33 -0300

    atualizando projeto

--- scripts/deploy.ps1
+++ scripts/deploy.ps1
@@ -5,14 +5,15 @@ Write-Host "ðŸš€ Iniciando deploy..." -ForegroundColor Cyan
 minikube start --driver=docker
 
 # 2. Configura o ambiente Docker
-minikube docker-env | Invoke-Expression
+minikube docker-env
 
 # 3. ConstrÃ³i as imagens
-docker build -t doadores-frontend ./Frontend/
-docker build -t doadores-backend ./Backend/
+docker build -t doadores-frontend ./cadastro-doadores/src/Frontend/
+docker build -t doadores-backend ./cadastro-doadores/src/Backend/
+kubectl create configmap mysql-init --from-file=./helm-chart/db-init/init.sql
 
 # 4. Aplica as configuraÃ§Ãµes Kubernetes
-    kubectl apply -f ./helm-chart/
+helm install cadastro-doadores ./helm-chart
 
 Write-Host "âœ… Deploy concluÃ­do! Acesse:" -ForegroundColor Green
 minikube service list
